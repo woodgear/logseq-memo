3f1e2da222a0d8f968cb58cd42763b6d68ce37f5
cong <q1875486458@gmail.com>
2024-12-04 17:23:54 +0800

    fix some bug

--- index.js
+++ index.js
@@ -9,11 +9,15 @@ async function memo() {
     logseq.App.showMsg(`Page "${pageTitle}" created.`);
 }
 
-async function update_start() {
+function lq_now_str() {
     const now = new Date()
-    const now_str = `${now.getHours()}:${now.getMinutes()}`
+    const now_str = `${now.getHours().toString().padStart(2, '0')}:${now.getMinutes().toString().padStart(2, '0')}`
+    return now_str
+}
+async function update_start() {
+    const now_str = lq_now_str()
     const cur = await logseq.Editor.getCurrentBlock()
-    console.log("update start",cur.body,"|||",cur.content)
+    console.log("update start", cur.body, "|||", cur.content)
     const content = cur.content
     if (content == "") {
         console.log("empty block")
@@ -34,11 +38,10 @@ async function update_start() {
 }
 
 async function update_end() {
-    const now = new Date()
-    const now_str = `${now.getHours()}:${now.getMinutes()}`
     const cur = await logseq.Editor.getCurrentBlock()
+    const now_str = lq_now_str()
     const content = cur.content
-    console.log("update end",cur.body,"|||",cur.content)
+    console.log("update end", cur.body, "|||", cur.content)
     if (content == "") {
         console.log("empty block")
         await logseq.Editor.updateBlock(cur.uuid, `${now_str}`)
@@ -47,21 +50,24 @@ async function update_end() {
 
     // has start and end
     if (content.match(/^(\d{1,2}:\d{1,2})-(\d{1,2}:\d{1,2}).*/)) {
-        console.log("already has range",content)
+        console.log("already has range", content)
         const matched = content.match(/^(\d{1,2}:\d{1,2})-(\d{1,2}:\d{1,2})/)[2]
         const content_new = content.replace(matched, `${now_str}`)
         await logseq.Editor.updateBlock(cur.uuid, content_new)
         return
     }
     // only has start
-    if (content.match(/^\s*(\d{1,2}:\d{1,2})\s*.*/)) {
+    if (content.match(/^\s*(\d{1,2}:\d{1,2}\-?)\s*.*/)) {
         console.log("only has start")
-        const matched = content.match(/^(\d{1,2}:\d{1,2})/)[1]
-        const content_new = content.replace(matched, `${matched}-${now_str}`)
+        const matched = content.match(/^(\d{1,2}:\d{1,2}\-?)/)[1]
+        const matched_without_dash = matched.replace("-", "")
+        const replaced = `${matched_without_dash}-${now_str}`
+        console.log("matched", matched, replaced)
+        const content_new = content.replace(matched, replaced)
         await logseq.Editor.updateBlock(cur.uuid, content_new)
         return
     }
-    console.log("has no time",content)
+    console.log("has no time", content)
     // has no time
     await logseq.Editor.updateBlock(cur.uuid, `${now_str} ${content}`)
 }
